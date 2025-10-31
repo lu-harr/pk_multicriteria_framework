9c6e6dbd28ebdcb5bb7427c6bc4b021ded4b22dd
Lucinda Harrison <lucy.harrison.research@gmail.com>
2025-10-31 14:27:27 +0000

    resuss prodlim

--- pk_multicrit_shiny/resuss_202510.R
+++ pk_multicrit_shiny/resuss_202510.R
@@ -6,3 +6,14 @@ borneo_remainder = rbind(malaysia_shp, brunei_shp)
 borneo_sf = st_as_sf(borneo_remainder)
 
 st_write(borneo_sf, "borneo_MB.shp")
+
+row.match <- function (x, table, nomatch = NA) 
+{
+  if (inherits(table, "matrix")) 
+    table <- as.data.frame(table)
+  if (is.null(dim(x))) 
+    x <- as.data.frame(matrix(x, nrow = 1))
+  cx <- do.call("paste", c(x[, , drop = FALSE], sep = "\r"))
+  ct <- do.call("paste", c(table[, , drop = FALSE], sep = "\r"))
+  match(cx, ct, nomatch = nomatch)
+}
